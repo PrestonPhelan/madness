c869a64ad980f1920e07b2f9f1f0c42f43bec1a6
PrestonPhelan <preston.phelan@gmail.com>
2017-03-18 13:34:10 -0700

    Team class, can read from CSV

---
+++ bracket-generator/team.rb
@@ -0,0 +1,22 @@
+class Team
+  attr_reader :overall, :seed, :name, :conference
+
+  def self.create_all(filename)
+    teams = Hash.new { Array.new }
+    f = File.open(filename)
+    f.each_line do |row|
+      cells = row.split(",")
+      teams[cells[1]] += [Team.new(*cells)]
+    end
+
+    teams
+  end
+
+  def initialize(*params)
+    @overall = params[0].to_i
+    @seed = params[1].to_i
+    @name = params[2]
+    @conference = params[3].strip
+  end
+
+end
